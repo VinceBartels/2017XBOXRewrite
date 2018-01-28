b3bbcd9aad453313127c3f14a337e40e38de7c97
VinceBartels <VinceBartels@users.noreply.github.com>
2018-01-28 14:44:21 -0800

    Updated DriveTrain

--- src/org/usfirst/frc/team5137/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5137/robot/subsystems/DriveTrain.java
@@ -10,33 +10,60 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 public class DriveTrain extends Subsystem {
+	
 	Victor leftDriveMotor = RobotMap.leftDriveMotor;
 	Victor rightDriveMotor = RobotMap.rightDriveMotor;
 	Spark slideDriveMotor = RobotMap.slideDriveMotor;
 	ADXRS450_Gyro gyro = RobotMap.gyro;
 	
 	DifferentialDrive hotWheels = RobotMap.hotWheels;
-	double Kp = 0.3;
+	double Kp = 0.03;
 	
 	
 	protected void initDefaultCommand() {
-	setDefaultCommand(new ArcadeDrive());		
+		setDefaultCommand(new ArcadeDrive());		
+	}
+	
+	public double adjustJoystickValue(double joystick, double deadZone) {
+		double adjustedJoystick;
+		if (Math.abs(joystick) < deadZone) {
+			adjustedJoystick = 0;
+		} else {
+			adjustedJoystick = ((1 / (1 - deadZone)) * (joystick - deadZone));
+		}
+		return adjustedJoystick;
 	}
 	
 	public void arcadeDrive(Joystick jackBlack) {
-		slideDriveMotor.set(jackBlack.getRawAxis(0));
-		hotWheels.arcadeDrive(jackBlack.getRawAxis(1), jackBlack.getRawAxis(4));
-		}
+		double adjustedSlideJoystick = adjustJoystickValue(jackBlack.getRawAxis(0), .2);
+		double adjustedArcadeJoystick = adjustJoystickValue(jackBlack.getRawAxis(1), .2);
+		double adjustedTurnJoystick = adjustJoystickValue(jackBlack.getRawAxis(4), .2);
+		
+		
+		slideDriveMotor.set(adjustedSlideJoystick);
+		hotWheels.arcadeDrive(adjustedArcadeJoystick, adjustedTurnJoystick);
+	}
 	
 	public void tankDrive(Joystick jackBlack) {
-		hotWheels.tankDrive(jackBlack.getRawAxis(1), jackBlack.getRawAxis(5));
-	
+		double adjustedLeftJoystick = adjustJoystickValue(jackBlack.getRawAxis(1), .2);
+		double adjustedRightJoystick = adjustJoystickValue(jackBlack.getRawAxis(5), .2);
+		double adjustedSlideJoystick = adjustJoystickValue(jackBlack.getRawAxis(0), .2);
+		
+		hotWheels.tankDrive(adjustedLeftJoystick, adjustedRightJoystick);
+		slideDriveMotor.set(adjustedSlideJoystick);
+			
 	}
 	
 	public void driveStraight() {
 		double angle = gyro.getAngle();
-		double speed = -0.75;
+		double speed = -0.65;
 		hotWheels.arcadeDrive(speed, angle*Kp);
+		
+	}
+	
+	public void slideDrive() {
+		double speed = .25;
+		slideDriveMotor.set(speed);
 		
 	}
 	
